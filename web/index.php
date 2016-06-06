2590b7a4e5ad2c2bd67f47a6a6382e085dcc21b3
tom29739 <tom29739@users.noreply.github.com>
2016-06-06 20:15:35 +0000

    Modified.

--- web/index.php
+++ web/index.php
@@ -13,12 +13,12 @@ $app->post('/captcha-dev/generate/', function () {
     return 'Hello, this is the (future) generator!';
 });
 
-$app->get('/captcha-dev/verify/', function () {
-    return 'Hello, this is the (future) verifier!';
+$app->get('/captcha-dev/verify/', function () use ($app) {
+    return $app->json(array('match' => rand(0,1) == 1));
 });
 
-$app->get('/captcha-dev/version/', function () {
-    return ;
+$app->get('/captcha-dev/version/', function () use ($app) {
+    return $app->json(array('hash' => exec('git log --pretty="%H" -n1 HEAD')));
 });
 
 $app['debug'] = true;
