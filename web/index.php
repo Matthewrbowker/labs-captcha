7800e18786f0d1061a55ff877781349ae50bdee0
tom29739 <tom29739@users.noreply.github.com>
2016-06-06 21:52:27 +0000

    Made generator

--- web/index.php
+++ web/index.php
@@ -2,18 +2,22 @@
 // web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Gregwar\Captcha\CaptchaBuilder;
+
 $app = new Silex\Application();
+$builder = new CaptchaBuilder;
 
 // definitions
 $app->get('/', function () {
     return 'Hello!';
 });
 
-$app->post('/generate/', function () {
-    return 'Hello, this is the (future) generator!';
+$app->post('/captcha/', function () use ($app,$builder) {
+    $builder->build();
+    return $app->json(array('image' => $builder->output()));
 });
 
-$app->get('/verify/', function () use ($app) {
+$app->post('/captcha/', function () use ($app) {
     return $app->json(array('match' => rand(0,1) == 1));
 });
 
