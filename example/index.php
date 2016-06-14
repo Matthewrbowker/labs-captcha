3833ad7a36d9a912243a1d2e91173a88576c04eb
tom29739 <tom29739@users.noreply.github.com>
2016-06-14 17:26:40 +0000

    Staging server

---
+++ example/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Example captcha API implementation</title>
+</head>
+<body>
+    <h1>Example captcha API implementation</h1>
+    <div class="l-captcha"></div>
+    <script src="//tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+    <script src="../js/l-captcha.js"></script>
+</body>
+</html>
