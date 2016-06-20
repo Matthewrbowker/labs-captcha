9cd0a118371ebb4b1749ae1654947bb6ad5e1fc3
tom29739 <tom29739@users.noreply.github.com>
2016-06-20 22:12:45 +0000

    Stuff.

--- web/index.php
+++ web/index.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
 $builder = new CaptchaBuilder;
 $uuid = Uuid::uuid4();
-$ignoreAllEffects = true
+$ignoreAllEffects = true;
 $builder->setIgnoreAllEffects($ignoreAllEffects);
 $builder->build();
 
